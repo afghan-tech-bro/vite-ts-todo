d9164a1dc847822327c7c0d76a6b912fc3d15995
omid-jan <omid.345@gmail.com>
2021-12-11 19:53:08 -0500

    updated tsconfig, base app

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const value = ref('Hello');
+const value = ref(0);
 
-const logMessage = () => console.log('%cApp.vue line:6 value.value', 'color: #007acc;', value.value);
+const handleButtonClick = () => {
+  value.value += 1;
+};
 </script>
 
 <template lang="pug">
 div
-  button(@click="logMessage")
-    | {{ value }}
-router-view
+  button(@click="handleButtonClick")
+    | Clicks: {{ value }}
+  router-view
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
