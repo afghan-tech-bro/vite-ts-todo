b2edb50112d3e7fd03ba1f816812a413aef69fe7
omid-jan <omid.345@gmail.com>
2021-12-11 22:20:42 -0500

    firebase init

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import authStore from '@/store/auth';
+
+const auth = authStore();
+auth.init();
+</script>
+
 <script lang="ts">
 export default {
   name: 'app-base',
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,11 @@
 <template lang="pug">
-p This text should update the site title
-input(v-model="title")
-br
-hr
-br
 demo-component
 </template>
 
-<script lang="ts" setup>
-const title = ref('Hola mundo!');
-useHead({ title });
+<script lang="ts">
+export default {
+  name: 'index-page',
+};
 </script>
 
 <route lang="yaml">
